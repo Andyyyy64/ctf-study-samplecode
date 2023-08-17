a5f54647dadd85ec5ee0a76fcfc60bd794adfa27
andy <takuyarossi77@gmail.com>
2023-08-17 21:57:47 +0900

    redacted

--- riv/Dynamic-Analysis/Exsample_4_ltrace.c
+++ riv/Dynamic-Analysis/Exsample_4_ltrace.c
@@ -1,4 +1,4 @@
-//find hardcoded strings using ltrace
+// ハードコードされた文字列をltraceで見つける問題
 
 #include <stdio.h>
 #include <string.h>
@@ -9,8 +9,8 @@ int main() {
 
     puts("Please input the passphrase");
     fgets(buf, sizeof(buf), stdin);
-    strtok(buf, "\n"); // Remove newline character
-    if(!strcmp(buf, key)) { // Compare the two strings
+    strtok(buf, "\n"); // 改行コードを削除
+    if(!strcmp(buf, key)) { // 二つの文字列を比較
         puts("Congratulations! your flag is ctf4b{trace you life}");
     } else {
         puts("invalid inputs");
@@ -20,16 +20,17 @@ int main() {
 }
 
 /*
-ltrace -> Used to trace library operations. Captures the operations when a program calls functions from dynamic libraries (like libc).
+ltrace -> ライブラリのトレースに使用する。プログラムの動的ライブラリ(libcなど)の関数を呼び出す際の操作をキャッチします５
 
-When tracing this program's binary using ltrace, the result is:
+このプログラムのバイナリをltraceでトレースしたさいの結果
 
 puts("Please input the passphrase"Please input the passphrase
 )                                       = 28
 fgets(test
-"test\n", 32, 0x7f2bb6d85980)                                       = 0x7fffc68a1b00
+"test\n", 32, memory!!)                                       = 0x7fffc68a1b00
 strtok("test\n", "\n")                                                    = "test"
 strcmp("test", "demodesufuckyou!!!")                                      = 16
 invalid inputs
 +++ exited (status 0) +++
 */
+
